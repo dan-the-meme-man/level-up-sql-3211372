20caf00c56a8e2a968da588c97184ca5bc8520aa
Dan DeGenaro <45244059+dan-the-meme-man@users.noreply.github.com>
2023-06-13 18:40:06 +0000

    4 and 5.

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,20 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+SELECT * FROM Customers
+WHERE FirstName = 'Taylor'
+AND LastName = 'Jenkins'
+AND City = 'Washington'
+;
+
+UPDATE Customers
+SET
+  Address = '74 Pine Street',
+  City = 'New York',
+  State = 'NY'
+WHERE CustomerID = 26;
+
+SELECT * FROM Customers
+WHERE FirstName = 'Taylor'
+AND LastName = 'Jenkins'
+;
