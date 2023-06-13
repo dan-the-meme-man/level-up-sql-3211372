6e6b6d60ff48ad7293a6eddca5d501a32189d7a7
Dan DeGenaro <45244059+dan-the-meme-man@users.noreply.github.com>
2023-06-13 18:10:58 +0000

    ;

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -3,4 +3,4 @@
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
 SELECT FirstName, LastName, Email FROM Customers
-ORDER BY LastName
+ORDER BY LastName;
