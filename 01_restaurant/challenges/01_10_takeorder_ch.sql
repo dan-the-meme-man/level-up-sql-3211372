69b7f9a3a0e899bafa84dc46c05750705334ccdb
Dan DeGenaro <45244059+dan-the-meme-man@users.noreply.github.com>
2023-06-14 20:36:00 +0000

    10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,17 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+INSERT INTO Orders(
+  CustomerID,
+  OrderDate
+) VALUES (
+  (SELECT CustomerID FROM Customers
+    WHERE FirstName = 'Loretta'
+    AND LastName = 'Hundey'
+    AND Address = '6939 Elka Place'
+  ),
+  '2022-09-20 14:00:00'
+);
+
+INSERT INTO OrdersDishes(OrderID, DishID)
+VALUES (1001, 4), (1001, 7), (1001, 20);
