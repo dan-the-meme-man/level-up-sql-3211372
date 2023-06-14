a523c3b18714b9b9f7af7b742abaad139e9f9d8a
Dan DeGenaro <45244059+dan-the-meme-man@users.noreply.github.com>
2023-06-14 22:12:51 +0000

    Library 1.

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,13 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+SELECT
+  (SELECT COUNT() FROM Books
+   WHERE Title = 'Dracula')
+  
+  -
+
+  (SELECT COUNT() FROM Loans
+   WHERE BookID IN (SELECT BookID FROM Books WHERE Title = 'Dracula')
+   AND ReturnedDate IS NULL)
+   
+  AS AvailableCopies;
