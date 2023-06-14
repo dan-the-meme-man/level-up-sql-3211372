a9fd9108436d0f86618104651bb62ff33dbef81e
Dan DeGenaro <45244059+dan-the-meme-man@users.noreply.github.com>
2023-06-14 20:16:15 +0000

    7 and 8.

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,13 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+SELECT * FROM Customers
+WHERE Email = 'atapley2j@kinetecoinc.com';
+
+INSERT INTO Responses(
+  ID, PartySize
+) VALUES (
+  (SELECT CustomerID
+   FROM Customers
+   WHERE Email = 'atapley2j@kinetecoinc.com'), 4
+);
