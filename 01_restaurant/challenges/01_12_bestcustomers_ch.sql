1023dd6199c6e1f313fc2b37f157d9e509179e3d
Dan DeGenaro <45244059+dan-the-meme-man@users.noreply.github.com>
2023-06-14 21:39:42 +0000

    12.

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,7 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+SELECT COUNT(OrderID) as OrderCount, CustomerID
+FROM Orders
+GROUP BY CustomerID
+ORDER BY OrderCount DESC;
